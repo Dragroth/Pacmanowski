c602a159b8e085a76bc518a086b2f7bfe5d1b873
Dragroth <werander.k@gmail.com>
2022-05-28 14:17:07 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+# screen resolution, change if you need it
+WIDTH, HEIGHT = 448, 596
+FPS = 60
+
+# colors 
+BLACK = (0,0,0)
+ORANGE =  (170, 130, 58)
+
+# fonts
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+# player settings 
+# enemy settings
