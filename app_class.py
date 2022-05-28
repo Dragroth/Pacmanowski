396d78b99d210cddc3b64cb030fe58ada58efaec
Dragroth <werander.k@gmail.com>
2022-05-28 16:45:46 +0200

    resolution and maze improvements

--- app_class.py
+++ app_class.py
@@ -12,6 +12,13 @@ class App:
         self.running = True
         self.state = 'start'
 
+        
+        self.cell_width = MAZE_WIDTH//28
+        self.cell_height = MAZE_HEIGHT//30
+
+
+        self.load()
+
     def run(self):
         while self.running:
             # states
@@ -40,6 +47,17 @@ class App:
             pos[0] = pos[0]-text_size[0]//2
             pos[1] = pos[1]-text_size[1]//2
         self.screen.blit(text, pos)
+
+    def load(self):
+        self.background = pygame.image.load('/home/gkk/Documents/Python/Pacmanowski/maze.png')
+        self.background = pygame.transform.scale(self.background, (MAZE_WIDTH, MAZE_HEIGHT))
+
+    def draw_grid(self):
+        for x in range(WIDTH//self.cell_width):
+            pygame.draw.line(self.background, GREY, (x*self.cell_width, 0), (x*self.cell_width, HEIGHT))
+        
+        for x in range(HEIGHT//self.cell_height):
+            pygame.draw.line(self.background, GREY, (0, x*self.cell_height), (WIDTH, x*self.cell_height))
 
 
 ######### START FUNCTIONS #########
@@ -77,5 +95,9 @@ class App:
 
 
     def playing_draw(self):
-        self.screen.fill(RED)
+        self.screen.fill(BLACK)
+        self.screen.blit(self.background, (TOP_BOTTOM_MARGIN//2, TOP_BOTTOM_MARGIN//2))
+        self.draw_grid()
+        self.draw_text(f"CURRENT SCORE: {0}", [60, 0], 18, WHITE, START_FONT)
+        self.draw_text(f"HIGH SCORE: {0}", [WIDTH//2+60, 0], 18, WHITE, START_FONT)
         pygame.display.update()
