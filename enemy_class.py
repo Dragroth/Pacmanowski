917657231f7f4658a669a06400f6a43746543e7c
Dragroth <werander.k@gmail.com>
2022-05-29 19:13:42 +0200

    enemy development

--- enemy_class.py
+++ enemy_class.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 from settings import *
 
 vec = pygame.math.Vector2
@@ -12,15 +12,35 @@ class Enemy:
         self.radius = self.app.cell_width//2.3
         self.number = number
         self.color = self.set_color()
-        self.direction = vec(1,0)
+        self.direction = vec(1 ,0)
         self.personality = self.set_personality()
 
     def update(self):
         self.pix_pos += self.direction
+        if self.time_to_move:
+            self.move()
 
 
     def draw(self):
         pygame.draw.circle(self.app.screen, self.color, (int(self.pix_pos.x), int(self.pix_pos.y)) , self.radius)
+
+    def time_to_move(self):
+        """"Checks whether it's okay to change direction, to stay in grid"""
+        if int(self.pix_pos.x+TOP_BOTTOM_MARGIN//2) % self.app.cell_width == 0:
+            if self.direction == vec(STEP,0) or self.direction == vec(-STEP,0):
+                return True
+        if int(self.pix_pos.y+TOP_BOTTOM_MARGIN//2) % self.app.cell_height == 0:
+            if self.direction == vec(0,STEP) or self.direction == vec(0,-STEP):
+                return True
+        return False
+
+    def move(self):
+        if self.personality == "random":
+            self.direction = self.get_random_direction()
+
+    def get_random_direction(self):
+        while True:
+            number = random.random()
 
     def get_pix_pos(self):
         """Gets pixel position based on initial grid position, passed when creating new instance"""
@@ -45,3 +65,5 @@ class Enemy:
             return "random"
         else:
             return "scared"
+
+    
